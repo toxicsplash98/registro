81404556a49ad40cac233e5d15ed60a9bd29a217
aguzuy <aguszarzur9@gmail.com>
2025-08-14 19:30:24 -0300

    added deleted functions

--- validacion.js
+++ validacion.js
@@ -5,6 +5,13 @@ let contrasena = document.getElementById('contrasena').value;
 let repetirContrasena = document.getElementById('repetir-contrasena').value;
 let terminos = document.getElementById('terminos').checked;
 
+function showAlertSuccess() {
+    document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+    document.getElementById("alert-danger").classList.add("show");
+}
 
 if (nombre && apellido && email && contrasena && repetirContrasena && 
    contrasena.length >= 6 && contrasena === repetirContrasena && terminos) {
